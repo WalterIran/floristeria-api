3b923252a694a36842dd87a1f3c3d0f8543ecff0
WalterIran <amadorwalter3418@gmail.com>
2022-02-22 11:02:14 -0600

    working on orders

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/users', require('./users/users.route'));
 router.use('/auth', require('./auth/auth.route'));
+router.use('/orders', require('./orders/orders.route'));
 
 module.exports = router;
---
+++ routes/v1/orders/orders.route.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const passport = require('passport');
+const validatorHandler = require('./../../../middlewares/validator.handler');
+
+router.get('/pending-orders/:id', );
+
+module.exports = router;

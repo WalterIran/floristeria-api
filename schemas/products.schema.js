3aeba2cfd84fffefb9184d9466d5a887438064a3
Jorge Salgado <68752578+JSalgado17@users.noreply.github.com>
2022-02-25 21:21:42 -0600

    CrudAllsProduct

--- schemas/products.schema.js
+++ schemas/products.schema.js
@@ -3,9 +3,16 @@ const Joi = require('joi');
 //Types
 const id = Joi.number().positive();
 const productName = Joi.string().max(100);
-const status = Joi.string().valid('ACT','INA');
+const productImgUrl = Joi.string().max(2500);
+const productDescriptionTitle = Joi.string().max(255);
+const productDescription = Joi.string().max(800);
 const price = Joi.number().positive().precision(2);
+const status = Joi.string().valid('ACT','INA');
+const discount = Joi.number().positive().precision(2);
+const discountExpirationDate = Joi.date();
+const createdAt = Joi.date();
 const updatedAt = Joi.date();
+const totalRating = Joi.number().positive().precision(2);
 
 //Schemas
 const productIdSchema = Joi.object({
@@ -14,10 +21,31 @@ const productIdSchema = Joi.object({
 
 const createProductSchema = Joi.object({
     productName: productName.required(),
+    productDescription: productDescription.required(),
+    productDescriptionTitle: productDescriptionTitle.required(),
+    productImgUrl,
+    price,
+    status,
+    discount,
+    discountExpirationDate,
+    totalRating,
+    createdAt,
+    updatedAt
+
+});
+
+const productDeleteSchema = Joi.object({
+    id: id.required()
 });
 
 const updateProductSchema = Joi.object({
-    productName
+    productName,
+    productImgUrl,
+    productDescriptionTitle,
+    productDescription,
+    price,
+    discount,
+    totalRating
 });
 
-module.exports = {productIdSchema};
+module.exports = {productIdSchema, productDeleteSchema, createProductSchema, updateProductSchema};

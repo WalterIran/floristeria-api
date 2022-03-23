36497c7580003d512cad600d1ac439a76357f0ff
Jorge Salgado <68752578+JSalgado17@users.noreply.github.com>
2022-03-22 22:20:22 -0600

    CrudTags

---
+++ schemas/tags.schema.js
@@ -0,0 +1,31 @@
+const Joi = require('joi');
+
+//Types.
+const id = Joi.number().positive();
+const tagName = Joi.string().max(255);
+const tagDescription = Joi.string().max(1500);
+const discount = Joi.number().positive().precision(2).allow(null);
+const discountExpirationDate = Joi.date().allow(null);
+
+//Schemas
+
+const TagsIdSchema =   Joi.object({
+    tagId: id.required()
+});
+
+const createTagsSchema = Joi.object({
+    tagName: tagName.required(),
+    tagDescription: tagDescription.required(),
+    discount,
+    discountExpirationDate
+});
+
+const updateTagsSchema = Joi.object({
+    tagName,
+    tagDescription,
+    discount,
+    discountExpirationDate
+});
+
+
+module.exports = {TagsIdSchema, updateTagsSchema, createTagsSchema};
